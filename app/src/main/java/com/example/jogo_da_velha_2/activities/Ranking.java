d2dba506fde701d02d225472041dac27f0886b6a
Antony-Marcos <142536686+Antony-Marcos@users.noreply.github.com>
2024-06-01 19:07:44 -0300

    Update Ranking.java

--- app/src/main/java/com/example/jogo_da_velha_2/activities/Ranking.java
+++ app/src/main/java/com/example/jogo_da_velha_2/activities/Ranking.java
@@ -86,7 +86,7 @@ public class Ranking extends AppCompatActivity {
                             textViewsPontuacao[3].setText(String.valueOf(obj.getInt("points")));
                             break;
                         case 4:
-                            textViewsRank[4].setText(obj.getInt("placing"));
+                            textViewsRank[4].setText(obj.getString("placing"));
                             textViewsNome[4].setText(obj.getString("user"));
                             textViewsPontuacao[4].setText(String.valueOf(obj.getInt("points")));
                             break;
